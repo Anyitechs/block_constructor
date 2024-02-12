5cf1c63871746c28ea2226ce0af9b4f9d4dfd16f
Ifeanyichukwu <ifeanyichukwu@mojapay.io>
2024-02-12 17:13:23 +0100

    feat: completed the block constructor implementation

---
+++ block_constructor.py
@@ -0,0 +1,82 @@
+import csv
+
+'''Block Constructor Challenge.
+- This Progam reads a CSV file and constructs a valid block off the file.
+'''
+class BlockConstructor:
+    def __init__(self, weight):
+        self.weight = weight
+    
+    '''This method reads the CSV file and returns the data
+    '''
+    def read_mempool_csv(self):
+        mempool_transactions = []
+        try:
+            with open('mempool.csv', mode='r') as file:
+                csv_file = csv.reader(file)
+                for lines in csv_file:
+                    for line in lines:
+                        mempool_transactions.append(line.split(','))
+                        # print(line.split(','))
+            return mempool_transactions
+        except FileNotFoundError as e:
+            print(e)
+
+
+    """This method gets the transaction with highest fees, to be included in a block.
+     - TODO: Maximize the transaction fee, by selecting the transaction with highest fees 
+    """
+    def get_highest_transaction_fees(self, mempool_transaction_fees):
+        highest_transaction_fee = mempool_transaction_fees[0]
+        high_fee_transactions = []
+        for transaction in mempool_transaction_fees:
+            if transaction > highest_transaction_fee:
+                highest_transaction_fee = transaction
+        
+        high_fee_transactions.append(highest_transaction_fee)
+        print(high_fee_transactions)
+
+        # for transaction in mempool_transaction_fees:
+        #     if 
+
+
+    '''This method constructs a valid block, maximizing the transaction fee and block weight
+    '''
+    def construct_block(self):
+        mempool_transactions = self.read_mempool_csv()
+        transaction_fees = []
+        parent_transactions = {}
+        block_transactions = []
+
+        for tx_index, transaction in enumerate(mempool_transactions):
+            # Get all transactions with a valid parent and store them in a list
+            if transaction[3] != "":
+                parent_transactions[tx_index] = transaction[3]
+
+        for tx_index, transaction in enumerate(mempool_transactions):
+            # Ensures only transactions with valid parents are included in the block
+            if transaction[3] != "" and transaction[0] in parent_transactions.values():
+                if self.weight > int(transaction[2]):
+                    block_transactions.append(parent_transactions[tx_index])
+                    block_transactions.append(transaction[0])
+                    self.weight -= int(transaction[2])
+                else:
+                    raise Exception("Maximum block size exceeded. You can't add more transactions to this block.")
+
+        
+        print(block_transactions[0])
+        print(block_transactions[1])
+        print(block_transactions[2])
+        print(block_transactions[3])
+
+
+
+def main():
+    max_block_weight = 4000000
+    block_constructor = BlockConstructor(max_block_weight)
+    block_constructor.construct_block()
+
+
+
+if __name__ == "__main__":
+    main()
